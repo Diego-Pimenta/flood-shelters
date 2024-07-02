9497f1417f73289b7132662cab7a1ac21188e4e7
Diego-Pimenta <diegopimenta888@gmail.com>
2024-07-02 15:09:43 -0300

    DB connection established

--- src/main/java/com/compass/Main.java
+++ src/main/java/com/compass/Main.java
@@ -1,7 +1,23 @@
 package com.compass;
 
-public class Main {
-    public static void main(String[] args) {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
+public class Main {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = null;
+        EntityManager em = null;
+
+        try {
+            emf = Persistence.createEntityManagerFactory("myPU");
+            em = emf.createEntityManager();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (em != null) em.close();
+            if (emf != null) emf.close();
+        }
     }
 }
